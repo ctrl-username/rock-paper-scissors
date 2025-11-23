b1a7b489ee4c9f8c1ac96ea0c3f8c97b526a40f3
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-11-23 19:01:49 +0100

    implement get human choice function

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let humanScore = 0;
 let computerScore;
 let humanSelection;
 let computerSelection;
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
@@ -11,9 +12,17 @@ function getComputerChoice() {
   } else if (randomNumber === 2) {
     computerSelection = "scissors";
   }
-  console.log(randomNumber, computerSelection);
 }
-function getHumanChoice() {}
+
+function getHumanChoice() {
+  humanSelection = prompt(
+    "pick an option between rock, paper or scissors"
+  ).toLowerCase();
+}
+
 function playRound() {}
 
 playRound(humanSelection, computerSelection);
+getHumanChoice();
+getComputerChoice();
+console.log(computerSelection, humanSelection);
