af6001a90ecacf8bb3d16a0e506daf2daa9f18dc
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-11-23 18:35:48 +0100

    created needed functions and variables

---
+++ main.js
@@ -0,0 +1,14 @@
+let humanScore = 0;
+let computerScore;
+let humanSelection;
+let computerSelection;
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+  }
+  console.log(randomNumber);
+}
+function getHumanChoice() {}
+function playRound() {}
+
+playRound(humanSelection, computerSelection);
