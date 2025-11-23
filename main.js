716b9737337459d5e78fc3bde7e86eb9871c0920
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-11-23 18:52:32 +0100

    implement getcomputer choice function

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ let computerSelection;
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   if (randomNumber === 0) {
+    computerSelection = "rock";
+  } else if (randomNumber === 1) {
+    computerSelection = "paper";
+  } else if (randomNumber === 2) {
+    computerSelection = "scissors";
   }
-  console.log(randomNumber);
+  console.log(randomNumber, computerSelection);
 }
 function getHumanChoice() {}
 function playRound() {}
